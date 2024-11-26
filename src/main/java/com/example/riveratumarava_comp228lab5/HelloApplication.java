e31419802a4b1a31a934e671c14fe513bc08cdef
Kelsy Rivera <keanri@outlook.com>
2024-11-26 17:59:20 -0500

    fxml labels and formatting

--- src/main/java/com/example/riveratumarava_comp228lab5/HelloApplication.java
+++ src/main/java/com/example/riveratumarava_comp228lab5/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 1000);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 900);
         stage.setTitle("Automobile Repair Shop");
         stage.setScene(scene);
         stage.show();
